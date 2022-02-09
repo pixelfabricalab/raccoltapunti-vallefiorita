4c44d88242cfc858e724acd4da28b06ec969b244
Andrea Coi <andrea.coi@outlook.it>
2022-02-09 12:05:05 +0100

    scrittura funzione per OCR ok

--- upload.php
+++ upload.php
@@ -1,4 +1,26 @@
 <?php
+
+function ocrFile($imagefile) {
+  $curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'http://89.148.181.23:9090/analizza',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => array('image'=> new CURLFILE($imagefile),'modo' => '3','engine' => '3'),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+  return $response;
+}
+
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["formFileLg"]["name"]);
 $uploadOk = 1;
