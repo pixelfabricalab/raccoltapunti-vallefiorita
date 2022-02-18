02f9be53b595250ed9a59682eb16e7c2d27b7ac6
Andrea Coi <andrea.coi@outlook.it>
2022-02-18 15:40:40 +0100

    strip linee scontrino per fine riga si leggerà ogni riga singolarmente.

--- frontend/controllers/ScontrinoController.php
+++ frontend/controllers/ScontrinoController.php
@@ -83,8 +83,12 @@ class ScontrinoController extends Controller
                 $model->datacattura = date('d-m-Y H:i:s');
                 $model->nomefile = './uploads/scontrini/' . hash('sha256', $filename . time()). '.'. $extension;
                 $response = $model->scansionaFile($model->nomefile);
-                $righe = $model->esplodiRighe($response);
-                var_dump($response);
+                $fineriga = $model->checkFineRiga($response);
+                //var_dump($response);
+                //var_dump($fineriga);
+                //die;
+                $righe = $model->esplodiRighe($response, PHP_EOL);
+                var_dump($righe);
                 die;
                 if ($model->save()) {
                     return $this->redirect(['view', 'id' => $model->id]);
