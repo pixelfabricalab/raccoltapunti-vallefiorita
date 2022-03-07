1663f0c98cc85218528f37aa642f6c614876b11b
Andrea Coi <andrea.coi@outlook.it>
2022-03-07 10:33:10 +0100

    Cambio nome app

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,11 +2,11 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+$this->title = 'Raccolta punti Vallefiorita';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron text-center bg-transparent">
+<div class="jumbotron text-center bg-transparent">
         <h1 class="display-4">Congratulations!</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
