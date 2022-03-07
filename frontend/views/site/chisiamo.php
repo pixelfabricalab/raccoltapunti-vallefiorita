7d4a5b95e3746ff59b90e79b76533ce56311742f
Andrea Coi <andrea.coi@outlook.it>
2022-03-07 16:00:34 +0100

    creazione vista chi-siamo

---
+++ frontend/views/site/chisiamo.php
@@ -0,0 +1,28 @@
+<?php
+
+/** @var yii\web\View $this */
+
+use yii\helpers\Html;
+
+$this->title = 'Chi Siamo';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="page-title">
+                    <h1><?= Html::encode($this->title) ?></h1>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="page-content">
+                <p><strong>Valle Fiorita</strong>&nbsp;nasce nell’ottobre del 1997 come piccola azienda produttrice di sandwich freschi in atm. In pochi anni diventa un’azienda leader sul mercato nazionale.</p>
+                <p>La produzione è interamente realizzata con farine selezionate. L’utilizzo di solo lievito naturale conferisce al prodotto elevata digeribilità e leggerezza. Inoltre l’utilizzo di olio extravergine d’oliva e sale iodato apporta pochissimi grassi saturi senza rinunciare al gusto. Questi elementi, abbinati ad una cottura su pietra per alcuni dei prodotti più ricercati, <strong>offrono al consumatore un’esperienza ricca dei sapori e dei profumi della nostra terra.</strong></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
