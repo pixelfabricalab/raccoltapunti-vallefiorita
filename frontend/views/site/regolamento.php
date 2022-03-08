4302bcb84a1d9057042f52aa8c36283e53967e47
Andrea Coi <andrea.coi@outlook.it>
2022-03-08 11:50:06 +0100

    creazione viste regolamento e premi

---
+++ frontend/views/site/regolamento.php
@@ -0,0 +1,30 @@
+<?php
+
+/** @var yii\web\View $this */
+
+use yii\helpers\Html;
+
+$this->title = 'Regolamento';
+?>
+<div class="site-about container">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="page-title">
+                    <h1><?= Html::encode($this->title) ?></h1>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="page-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras a orci in lacus faucibus porttitor. Nulla interdum, erat id pretium tincidunt, diam quam tristique nisi, vitae convallis nisi dui id nibh. Phasellus vehicula enim nisl, sit amet dapibus risus vestibulum sit amet. Curabitur aliquam nec mi sit amet dictum. Nulla tincidunt laoreet sem, quis convallis diam pretium vel. Duis et sagittis diam. Quisque vulputate facilisis luctus.</p>
+                <p>Phasellus ultrices nisl eget justo dignissim, in porta metus porttitor. Suspendisse pretium, nibh iaculis tempor convallis, massa dolor bibendum dolor, eu congue tortor mi vel eros. Nam tortor mauris, semper eget mollis at, lobortis et dolor. Quisque tincidunt tellus non pharetra aliquam. Vestibulum vel quam ex. Nullam bibendum orci ex, vitae suscipit felis porttitor sit amet. Phasellus vel leo nisl. Etiam fringilla eros aliquet sem feugiat, sed commodo turpis dapibus. Curabitur pellentesque ante vel tempor luctus. Sed eget mollis arcu. Quisque semper in massa a aliquam.</p>
+                <p>Nam luctus eros purus, in vulputate arcu mollis molestie. Donec diam mauris, ultrices in pulvinar in, lobortis sed dui. Fusce tincidunt fermentum nisl, ac volutpat velit elementum ut. Morbi in enim eget urna iaculis molestie. Maecenas porta metus eu nisl suscipit, ut pellentesque odio euismod. Sed luctus volutpat urna quis finibus. Nam ultricies orci feugiat nunc vulputate, nec pellentesque mauris tempus. Cras rutrum vitae enim nec maximus. Sed rutrum volutpat aliquet.</p>
+                <p>Integer scelerisque mattis sodales. Quisque cursus metus nibh, sed rhoncus nisi efficitur id. Nam ac rutrum orci. Donec cursus nulla eget nibh consequat, ut porttitor est fringilla. Fusce iaculis mauris auctor neque iaculis, vel iaculis elit porta. Integer ultrices in sem quis convallis. Morbi condimentum vulputate sollicitudin. Phasellus tempus pellentesque nunc. Integer tempor metus nec augue pulvinar cursus. Vivamus vitae vestibulum enim, a rutrum mauris. Donec sit amet venenatis neque. Aenean interdum placerat urna sit amet sollicitudin.</p>
+                <p>Duis nulla nisl, blandit quis sapien eu, dignissim feugiat sapien. Proin augue diam, laoreet eu facilisis ut, tristique quis orci. Cras tincidunt urna quis quam malesuada vulputate. Pellentesque suscipit, erat facilisis accumsan lacinia, mi justo iaculis erat, id maximus neque massa sit amet massa. Etiam mattis faucibus ornare. Morbi scelerisque ipsum eu augue vulputate, a suscipit libero mattis. Proin non consectetur massa. Donec interdum leo et dictum iaculis. Praesent cursus sapien non augue cursus interdum.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
