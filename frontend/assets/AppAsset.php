fc408984ed843915ac3da8453a62f12d823be37a
Andrea Coi <andrea.coi@outlook.it>
2022-03-09 10:12:50 +0100

    aggiunge font-awesome ai fogli di stile

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,6 +13,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
     ];
     public $js = [
     ];
