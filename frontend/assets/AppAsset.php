adfde354d545fbaab6d46448f22dc6f3e3bfa2c6
Andrea Coi <andrea.coi@outlook.it>
2022-03-21 16:12:27 +0100

    inserisce axios all'interno degli assets

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -16,6 +16,7 @@ class AppAsset extends AssetBundle
         'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
     ];
     public $js = [
+        'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js',
         'https://unpkg.com/vue@3',
         'js/main.js',
         'js/gioco.js'
