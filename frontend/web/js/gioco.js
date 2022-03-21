81e26f8f45d497b760534201cd180cfe118eb358
Andrea Coi <andrea.coi@outlook.it>
2022-03-21 16:13:38 +0100

    aggiunge i metodi per le chiamate rest

--- frontend/web/js/gioco.js
+++ frontend/web/js/gioco.js
@@ -1,8 +1,23 @@
 Vue.createApp({
     data() {
       return {
-        //message: 'Hello Vue!',
         stepgioco: 0,
+        image: null,
       }
-    }
+    },
+    methods: {
+        uploadFile() {
+            this.image = this.$refs.file.files[0];
+        },
+        submitFile(){
+            const formData = new FormData();
+            formData.append('file', this.image);
+            const headers = {'Content-Type':'multipart/form-data'};
+            axios.post('/raccoltapunti-vallefiorita/frontend/web/index.php?r=scontrino/create', formData, {headers}).then((res) => {
+                res.data.files;
+                res.status;
+                stepgioco++;
+            })
+        }
+     },
   }).mount('#gioco')
