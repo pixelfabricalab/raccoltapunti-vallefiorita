d58692e036c824432f2d4e7531a2844847cdb313
Andrea Coi <andrea.coi@outlook.it>
2022-04-28 12:22:43 +0200

    modifica view singolo scontrino

--- frontend/views/scontrino/view.php
+++ frontend/views/scontrino/view.php
@@ -4,43 +4,20 @@ use yii\helpers\Html;
 use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
-/* @var $model frontend\models\Scontrino */
+/* @var $model app\models\Scontrino */
 
-$this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Scontrinos', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = 'Scontrino n°'. $model->id;
+
 \yii\web\YiiAsset::register($this);
 ?>
-<div class="scontrino-view">
+<div class="scontrino-view container">
 
     <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
 
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
             'nomefile',
-            'hashnomefile',
-            'estensionefile',
-            'dimensionefile',
-            'proprietario_id',
-            'datacattura',
-            'rfscontrino',
-            'piva',
-            'ragionesociale',
-            'dataemissione',
-            'numerodocumento',
         ],
     ]) ?>
 
