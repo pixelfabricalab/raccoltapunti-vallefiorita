7ae116338078b9957bd6f2d6f14f8bba1e95635e
Andrea Coi <andrea.coi@outlook.it>
2022-04-28 12:10:59 +0200

    riscrive helper per scansione OCR scontrino

---
+++ frontend/components/ScontrinoHelper.php
@@ -0,0 +1,49 @@
+<?php
+    namespace frontend\components;
+
+    use yii\web\Response;
+
+    class ScontrinoHelper {
+        public function scanOCR($nomefile) {
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'http://89.148.181.23:9090/analizza',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array('image'=> new \CURLFILE($nomefile),'modo' => '3','engine' => '3'),
+            ));
+
+            $response_curl = curl_exec($curl);
+            // inizializzo response come un oggetto di tipo Response
+            $response = new Response;
+            // dichiaro che il formato della risposta che voglio ricevere è di tipo JSON
+            $response->format = Response::FORMAT_JSON;
+            // associo al contenuto di response la stringa generata precedentemente
+            $response->content = $response_curl;
+
+            curl_close($curl);
+            return $response;
+        }
+
+        public function validaJSON(string $response) {
+            if ($response != null) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        
+        public function isScontrinoValid($response) {
+            return true;
+        }
+
+        public function getScontrinoData($response) {
+            return true;
+        }
+    }
