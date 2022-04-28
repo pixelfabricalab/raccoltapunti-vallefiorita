c4614fef87062f03eaa2c29762b181c4218f41ab
Andrea Coi <andrea.coi@outlook.it>
2022-04-28 10:26:40 +0200

    ripristino form originale

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -1,65 +1,30 @@
 <?php
 
 /** @var yii\web\View $this */
-/** @var yii\bootstrap5\ActiveForm $form */
+/** @var yii\bootstrap4\ActiveForm $form */
 /** @var \frontend\models\SignupForm $model */
 
-use yii\bootstrap5\Html;
-use yii\bootstrap5\ActiveForm;
-use yii\jui\DatePicker;
+use yii\bootstrap4\Html;
+use yii\bootstrap4\ActiveForm;
 
-$this->title = 'Registrati';
+$this->title = 'Signup';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-signup container">
+<div class="site-signup">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Per registrarti e iniziare la raccolta punti compila i seguenti campi:</p>
+    <p>Please fill out the following fields to signup:</p>
 
     <div class="row">
-        <div class="col">
+        <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
-            <div class = "row">
-                <div class="col-md-4">
-                    <?= $form->field($model, 'nome')->textInput(['autofocus'=> true]) ?>
-                </div>
-                <div class="col-md-4">
-                    <?= $form->field($model, 'cognome') ?>
-                </div>
-                <div class="col-md-4">
-                    <?= $form->field($model, 'datanascita')->widget(DatePicker::classname(), [
-                        'language' => 'it',
-                        'dateFormat' => 'dd-MM-yyyy',
-                        'class' => 'form-control',
-                        'options' => array(
-                            'class' => 'form-control'			// textField size
-                        )
-                        ])->label('Data di nascita') ?>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <?= $form->field($model, 'username')->textInput() ?>
-                </div>
-                <div class="col-md-4">
-                    <?= $form->field($model, 'email')->textInput() ?>
-                </div>
-                <div class="col-md-4">
-                    <?= $form->field($model, 'password')->passwordInput() ?>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                <?= $form->field($model, 'trattamentodati')->checkboxList([
-                    1 => 'Acconsento al trattamento dei dati', 
-                ])->label('Trattamento dei dati generico'); ?>
-                <?= $form->field($model, 'marketing')->checkboxList([
-                    1 => 'Acconsento al trattamento dei dati ai fini marketing'
-                ])->label('Trattamento dei dati ai fini marketing'); ?>
-                <?= $form->field($model, 'profilazione')->checkboxList([
-                    1 => 'Acconsento al trattamento dei dati a fine di profilazione'
-                ])->label('Trattamento dei dati per profilazione e statistica'); ?>
-                </div>
-            </div>
+
+                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+
+                <?= $form->field($model, 'email') ?>
+
+                <?= $form->field($model, 'password')->passwordInput() ?>
+
                 <div class="form-group">
                     <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
                 </div>
