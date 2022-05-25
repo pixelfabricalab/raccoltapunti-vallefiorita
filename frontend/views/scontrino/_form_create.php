5becbac09237ecaa2dcf47988144749ab2f36579
Andrea Coi <andrea.coi@outlook.it>
2022-05-25 13:10:20 +0200

    crea dropdown per scegliere  i nuovi campi

--- frontend/views/scontrino/_form_create.php
+++ frontend/views/scontrino/_form_create.php
@@ -16,8 +16,11 @@ use yii\widgets\ActiveForm;
 ); ?>
 
     <?= $form->field($model, 'nomefile')->fileInput(['class' => 'form-control-file fileinput'])->label('Carica Scontrino') ?>
-
-    <div class="form-group">
+    <?= $form->field($model, 'modoscansione')->dropdownList(['1' => '1 - Segmentazione automatica della pagina con riconoscimento automatico dell\'orientamento pagina', '2' => '2 - Segmentazione automatica della pagina, nessun riconoscimento automatico', '3' => '3 - Segmentazione automatica completa'], ['prompt' => '---Seleziona tipo PSM---'])->hint('Inserisci la modalità con la quale vuoi eseguire la scansione')->label('Modo Scansione') ?>
+    <?= $form->field($model, 'enginescansione')->dropdownList(['1' => '1 - Utilizza l\'intelligenza artificiale per il riconoscimento delle singole righe', '2' => '2 - Utilizza l\'intelligenza artificiale per il riconoscimento delle singole righe, ma lascia a Tesseract la decisione se le cose si complicano', '3' => '3 - Modalità di default'], ['prompt' => '---Seleziona tipo LSTM---'])->hint('Inserisci la modalità con la quale vuoi eseguire la scansione')->label('Engine Scansione') ?>
+    <?= $form->field($model, 'dpiscansione')->dropdownList(['150' => 'Elabora lo scontrino a 150dpi', '200' => 'Elabora lo scontrino a 200dpi', '300' => 'Elabora lo scontrino a 300dpi', '400' => 'Elabora lo scontrino a 400dpi'], ['prompt' => '---Seleziona DPI---'])->hint('Inserisci i DPI con i quali vuoi eseguire la scansione, più è alto il numero più sarà lenta la scansione')->label('DPI Scansione') ?>
+    <?= $form->field($model, 'raddrizzascansione')->dropdownList(['0' => 'No', '1' => 'Si'], ['prompt' => '---Raddrizza Foto---'])->hint('Vuoi che il sistema provi a raddrizzare la tua foto per migliorare la scansione?')->label('Raddrizza Scansione')  ?>
+    <div>
         <?= Html::submitButton('Carica', ['class' => 'btn btn-success']) ?>
     </div>
 
