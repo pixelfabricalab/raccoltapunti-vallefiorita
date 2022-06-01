4395c58aca5a2150dc5d394c528f212ad98e91a6
Andrea Coi <andrea.coi@outlook.it>
2022-06-01 09:33:59 +0200

    abilita test mode per scansioni veloci

--- console/controllers/ScannerOcrController.php
+++ console/controllers/ScannerOcrController.php
@@ -15,24 +15,33 @@
           echo $search;
         }
 
-        public function actionScan() {
+        public function actionScan($test = false) {
           // inizializza i costruttori necessari per lo script
           $request = new Request;
           $logger = new LoggerHelper;
           $scanner = new ScontrinoHelper;
-
-          // array per dimensioni
-          $dimensioni_scansione = [1600,2200,2800,3200];
-          // array per modalità di segmentazione pagina
-          $modes_psm = [1,3];
-          // array per engine segmentazione pagina
-          $engines_lstm = [1,2,3,6,11,12];
-          // array per dpi scansione 
-          $dpis_scansione = [300,400,600];
-          // array con valori boolean: 0 e 1 per attivare e disattivare funzione di raddrizzamento automatico - non utilizzata su suggerimento di Ale. Non funziona bene.
-          $desks_foto = [0];
-          // numero di scansioni totali per file: 144
-
+          
+          // condizione per scansionare solo 1 modalità - necessaria per test
+          if ($test == true) {
+            $dimensioni_scansione = [3200];
+            $modes_psm = [3];
+            $engines_lstm = [3];
+            $dpis_scansione = [600];
+            $desks_foto = [0];
+          } else {
+            // array per dimensioni
+            $dimensioni_scansione = [1600,2200,2800,3200];
+            // array per modalità di segmentazione pagina
+            $modes_psm = [1,3];
+            // array per engine segmentazione pagina
+            $engines_lstm = [1,2,3,6,11,12];
+            // array per dpi scansione 
+            $dpis_scansione = [300,400,600];
+            // array con valori boolean: 0 e 1 per attivare e disattivare funzione di raddrizzamento automatico - non utilizzata su suggerimento di Ale. Non funziona bene.
+            $desks_foto = [0];
+            // numero di scansioni totali per file: 144
+          }
+          
           // array di estensioni di file valevoli per la scansione
           $extension_to_search = array('jpg','png','jpeg','gif');
 
@@ -49,7 +58,7 @@
           // inizializzo i contatori a 0 e a 1. mi servono per contare i file elaborati e i task per ogni file.
           $count = 0;
           $task = 1;
-
+          $attempts = 1;
           // primo ciclo - per ogni file presente nell'array files, verifica che si tratti di un'immagine
           foreach($files as $key => $value) {
             $ext = pathinfo($value, PATHINFO_EXTENSION);
@@ -82,7 +91,8 @@
                             $task++;
                           } else {
                             // esce con errore I/O se response->content è vuota o Null
-                            ExitCode::IOERR;
+                            $attempts++;
+                            echo $attempts;
                           }
                       }
                     }
@@ -94,8 +104,11 @@
                 // to fix: sposta il file in un'altra directory in modo che uploads risulti sempre vuota. NON FUNZIONA?
                 move_uploaded_file($dir_scontrini_da_scansionare.$value, "{$url}/frontend/web/uploads/elapsed/{$value}");
                 // todo: implementare sistema di salvataggio output alla fine di ogni task, implementare sistema di avviso mail e invio dell'allegato _batchocroutput.log tramite mail a me, ad Alessandro e a Fabrizio.
-                ExitCode::OK;
+                return ExitCode::OK;
             }
+          if ($attempts == 3) {
+            break;
+          }
         }
         echo "Conto totale file: {$count}";
         }
