8859e66b53e82cdd1358ae093b1bbfa455618ffa
Andrea Coi <andrea.coi@outlook.it>
2022-06-06 12:31:14 +0200

    modifica cartella per scrittura file di log

--- common/components/LoggerHelper.php
+++ common/components/LoggerHelper.php
@@ -9,7 +9,7 @@
             //$base = Yii::getAlias('@webroot') . '/uploads/scontrini/';
             $request = new Request;
             $url = $request->getBaseUrl();
-            $logfile = fopen("./uploads/scontrini/_uploads.log", "a") or die("Unable to open file!");
+            $logfile = fopen("./logs/_uploads.log", "a") or die("Unable to open file!");
             fwrite($logfile, $content);
             fclose($logfile);
             return true;
@@ -18,7 +18,7 @@
         public function logOCROutput($output) {
             $request = new Request;
             $url = $request->getBaseUrl();
-            $logfile = fopen("{$url}/uploads/scontrini/_ocroutput.log", "a") or die("Unable to open file!");
+            $logfile = fopen("{$url}/logs/_ocroutput.log", "a") or die("Unable to open file!");
             fwrite($logfile, $output);
             fclose($logfile);
             return true;
@@ -27,7 +27,16 @@
         public function logBatchOCROutput($output, $filename) {
             $request = new Request;
             $url = $request->getBaseUrl();
-            $logfile = fopen("{$url}/frontend/web/uploads/scontrini/{$filename}", "a") or die("Unable to open file!");
+            $logfile = fopen("{$url}/frontend/web/logs/{$filename}", "a") or die("Unable to open file!");
+            fwrite($logfile, $output);
+            fclose($logfile);
+            return true;
+        }
+
+        public function logCLIWorks($output, $filename) {
+            $request = new Request;
+            $url = $request->getBaseUrl();
+            $logfile = fopen("{$url}/frontend/web/logs/{$filename}", "a") or die("Unable to open file!");
             fwrite($logfile, $output);
             fclose($logfile);
             return true;
