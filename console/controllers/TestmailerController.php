cf8c8a240a5b3eeba993d7a6a2415f0585c8ab90
Andrea Coi <andrea.coi@outlook.it>
2022-06-08 15:13:07 +0200

    fix test mailer sender https://forum.yiiframework.com/t/swiftmailer-error-503/125559

--- console/controllers/TestmailerController.php
+++ console/controllers/TestmailerController.php
@@ -8,7 +8,7 @@ class TestmailerController extends Controller {
     public function actionSend() {
         Yii::$app->mailer
             ->compose('test')
-            ->setFrom([Yii::$app->params['supportEmail'] => Yii::$app->name . ' robot'])
+            ->setFrom('ocr@pixelfabrica.it')
             ->setTo('acandry90@gmail.com')
             ->setSubject('Mail di prova inviata da ' . Yii::$app->name)
             ->send();
