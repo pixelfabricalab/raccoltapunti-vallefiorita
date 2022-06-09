d7c8d66977e7c0e284fed7570d2d6f0a4ca47292
Andrea Coi <andrea.coi@outlook.it>
2022-06-09 09:25:49 +0200

    sopprime le funzioni nella vista scontrino singolo

--- frontend/views/scontrino/view.php
+++ frontend/views/scontrino/view.php
@@ -6,8 +6,6 @@ use yii\widgets\GridView;
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Scontrino */
-/* @var $datamodel frontend\models\ScontrinoData */
-/* @var $dataprodottimodel frontend\models\ProdottiScontrinoData */
 
 $this->title = 'Scontrino n°'. $model->id;
 
@@ -16,7 +14,6 @@ $this->title = 'Scontrino n°'. $model->id;
 <div class="scontrino-view container">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php if ($datamodel[0]->piva == NULL) : ?>
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
@@ -26,51 +23,10 @@ $this->title = 'Scontrino n°'. $model->id;
             'class' => 'alert-warning',
         ],
     ]) ?>
-    <?php else : ?>
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
             'nomefile',
         ],
     ]) ?>
-    <?php endif; ?>
-    <hr />
-    <h3> Dettagli Scontrino</h3>
-    <table id="w0" class="table table-striped table-bordered detail-view">
-        <thead>
-            <th>RF Scontrino</th>
-            <th>Numero Documento</th>
-            <th>Data Emissione</th>
-            <th>Ragione Sociale</th>
-            <th>Indirizzo</th>
-            <th>Provincia</th>
-            <th>Città</th>
-            <th>CAP</th>
-            <th>Telefono</th>
-            <th>P.IVA</th>
-            <th>P.IVA Valida?</th>
-            <th>P.IVA VIES?</th>
-        </thead>
-        <tbody>
-    
-
-        </tbody>
-    </table>
-    <hr />
-    <h3>Prodotti Scontrino</h3>
-    <table id="w0" class="table table-striped table-bordered detail-view">
-        <thead>
-            <th>Nome prodotto</th>
-            <th>Prezzo Prodotto</th>
-            <th>Tipo Prodotto</th>
-            <th>IVA Prodotto</th>
-            <th>Multiprodotto</th>
-            <th>Moltiplicatore</th>
-            <th>Punti per prodotto</th>
-            <th>Numero punti</th>
-        </thead>
-    </table>
-    <hr />
-    <h3>Output per OCR</h3>
-        <textarea rows="10" cols="100"><?= $datamodel[0]->outputocr ?></textarea>
 </div>
