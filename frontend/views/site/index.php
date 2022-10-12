f303688b1579cc4a513a2b3a734865c17430da68
Andrea Coi <andrea.coi@outlook.it>
2022-10-12 16:03:54 +0200

    modifica palette colori

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -19,7 +19,7 @@ $this->title = 'Raccolta punti - Demo App';
     <div class="body-content">
         <div class="row">
             <div class ="col-md-6 col-xs-12 col-sm-12">
-                <div class="bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+                <div class="bg-profility-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
                 <div class="my-3 py-3">
                     <h2 class="display-5">Premio 1</h2>
                     <p class="lead">Aggiudicati il premio con 150 punti.</p>
@@ -28,12 +28,12 @@ $this->title = 'Raccolta punti - Demo App';
                 </div>
             </div>
             <div class="col-md-6 col-xs-12 col-sm-12">
-                <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+                <div class="bg-profility-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
                     <div class="my-3 p-3">
-                    <h2 class="display-5">Premio 2</h2>
-                    <p class="lead">Aggiudicati il premio con 300 punti.</p>
+                    <h2 class="profility-color display-5">Premio 2</h2>
+                    <p class="lead profility-color">Aggiudicati il premio con 300 punti.</p>
                 </div>
-                <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+                <div class="bg-profility-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
             </div>
         </div>
     </div>
