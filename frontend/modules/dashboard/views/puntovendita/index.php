9767e737e2284076a01fb8ab3176f4790759a278
Vincent <vincent.veri@gmail.com>
2022-10-13 15:17:41 +0200

    visualizzamappa

--- frontend/modules/dashboard/views/puntovendita/index.php
+++ frontend/modules/dashboard/views/puntovendita/index.php
@@ -12,11 +12,42 @@ use yii\widgets\Pjax;
 $this->title = 'Punti vendita';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+ <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css"
+     integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14="
+     crossorigin=""/>
+
+ <!-- Make sure you put this AFTER Leaflet's CSS -->
+ <script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js"
+     integrity="sha256-o9N1jGDZrf5tS+Ft4gbIK7mYMipq9lqpVJ91xHSyKhg="
+     crossorigin=""></script>
+
 <div class="puntovendita-index">
 
     <p>
         <?= Html::a('Nuovo', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
+
+    <div class="mb-2">
+        <div id="map"></div>
+    </div>
+
+<style>
+    #map {height: 350px;}
+</style>
+<script>
+var map = L.map('map').setView([40.3513835, 18.1766978], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([40.3513835, 18.1766978]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+L.marker([40.3913835, 18.1966978]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.');
+</script>    
 
     <?php Pjax::begin(); ?>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
