75a92c60c879023c6bfc177a59030b6cf2df0e73
Andrea Coi <andrea.coi@outlook.it>
2023-02-22 15:28:21 +0100

    incorpora iframe powerbi

---
+++ backend/views/statistiche/index.php
@@ -0,0 +1,12 @@
+<?php
+    use yii\helpers\Html;
+    use yii\helpers\Url;
+    /* @var $this yii\web\View */
+    /* @var $dataProvider yii\data\ActiveDataProvider */
+
+    $this->title = "Statistiche generali";
+    $this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="statistiche-index">
+<iframe title="Test_Profility" width="100%" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiYzViMjAxNzYtYzRmYi00ZmE0LWJiYWUtOWI2YzdlM2U1MjRjIiwidCI6IjY1NGViNGY3LTc1YjAtNGQzNi04Y2E2LTU5NGVjOTAwZWJkMSIsImMiOjl9" frameborder="0" allowFullScreen="true"></iframe>
+</div>
