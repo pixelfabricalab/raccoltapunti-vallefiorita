3e0fd763eb6ed72e91791f2d2277f6b819f9f548
Andrea Coi <andrea.coi@outlook.it>
2023-02-28 14:33:10 +0000

    aggiunge vista partecipanti

---
+++ backend/views/statistiche/partecipanti.php
@@ -0,0 +1,12 @@
+<?php
+    use yii\helpers\Html;
+    use yii\helpers\Url;
+    /* @var $this yii\web\View */
+    /* @var $dataProvider yii\data\ActiveDataProvider */
+
+    $this->title = "Partecipanti";
+    $this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="statistiche-index">
+<iframe title="Partecipanti" width="100%" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiYTYxNjY4OGUtM2FjMi00ODlmLTgzNTQtOTU5ZTY0ODBmYjM4IiwidCI6IjY1NGViNGY3LTc1YjAtNGQzNi04Y2E2LTU5NGVjOTAwZWJkMSIsImMiOjl9" frameborder="0" allowFullScreen="true"></iframe>
+</div>
