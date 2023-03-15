fe7c0069a51771221f00a63d6f470544ec3743ab
Andrea Coi <andrea.coi@outlook.it>
2023-03-15 16:00:45 +0100

    inserisce parametri asprise test

--- common/config/params.php
+++ common/config/params.php
@@ -7,4 +7,8 @@ return [
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
     'endpoint_server_ocr' => 'http://192.168.10.4:8090/analizza',
+    'endpoint_server_aspriseocr' => 'https://ocr.asprise.com/api/v1/receipt',
+    'api_key_asprise' => 'TEST',
+    'recognizer' => 'IT',
+    'ref_no' => 'ocr_asprise_demo_pxl01',
 ];
