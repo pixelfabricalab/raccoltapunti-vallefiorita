bef0c587c61821c0b038ae83e7ff15786aa1e5fa
Andrea Coi <andrea.coi@outlook.it>
2023-03-16 23:17:48 +0100

    rivede template view singola scansione

--- frontend/modules/dashboard/views/scontrino/view.php
+++ frontend/modules/dashboard/views/scontrino/view.php
@@ -14,52 +14,63 @@ $this->title = 'Scontrino id n°'. $model->id;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="alert alert-success">
-    <h1>Questo scontrino vale <?=rand(0,10)?> punti!</h1>
+    <h2>Questo scontrino vale <?=rand(0,10)?> punti!</h2>
 </div>
 <hr />
 <h1>Dettaglio scontrino</h1>
-<div class="scontrino-view container">
-<h3 class="esercente title">Dati immagine scansionata</h3>
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'nomefile',
-            'data_caricamento',
-        ],
-    ]) ?>
-    <h3 class="esercente title">Dati esercente</h3>
-    <?= DetailView::widget([
-        'model' => $datamodel,
-        'attributes' => [
-            'rfscontrino',
-            'numerodocumento',
-            'dataemissione',
-            'ragionesociale',
-            'indirizzo',
-            'provincia',
-            'citta',
-            'cap',
-            'telefono',
-            'partitaiva',
-        ],
-    ]) ?>
-    <?= GridView::widget([
-        'dataProvider' => $provider_dataprodotti,
-		'emptyText' => 'Non esistono prodotti sullo scontrino.',
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            [ 
-                'header' => 'Nome prodotto',
-                'attribute' => 'nomeprodotto',
+<div class="scontrino-view container-fluid">
+    <div class="row">
+        <div class="col-md-3 col-sm-12">
+            <img src="<?= "/frontend/web/uploads/scontrini/". $model->hashnomefile . "." . $model->estensionefile ?>" width="300">
+        </div>
+        <div class="col-md-9 col-sm-12">
+    <h3 class="esercente title">Dati immagine scansionata</h3>
+        <?= DetailView::widget([
+            'model' => $model,
+            'attributes' => [
+                ['label' => 'File',
+                 'attribute' => function($model) {
+                    return $model->hashnomefile . "." . $model->estensionefile;
+                 }   
+                ],
+                'data_caricamento',
             ],
-            [
-                'header' => 'Prezzo Prodotto',
-                'attribute' => 'prezzo_prodotto',
+        ]) ?>
+        <h3 class="esercente title">Dati esercente</h3>
+        <?= DetailView::widget([
+            'model' => $datamodel,
+            'attributes' => [
+                'rfscontrino',
+                'numerodocumento',
+                'dataemissione',
+                'ragionesociale',
+                'indirizzo',
+                'provincia',
+                'citta',
+                'cap',
+                'telefono',
+                'partitaiva',
             ],
-            [
-                'header' => 'Numero punti',
-                'attribute' => 'numeropunti',
-            ],
-        ]]);
-        ?>
+        ]) ?>
+        <?= GridView::widget([
+            'dataProvider' => $provider_dataprodotti,
+            'emptyText' => 'Non esistono prodotti sullo scontrino.',
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+                [ 
+                    'header' => 'Nome prodotto',
+                    'attribute' => 'nomeprodotto',
+                ],
+                [
+                    'header' => 'Prezzo Prodotto',
+                    'attribute' => 'prezzo_prodotto',
+                ],
+                [
+                    'header' => 'Numero punti',
+                    'attribute' => 'numeropunti',
+                ],
+            ]]);
+            ?>
+        </div>
+    </div>
 </div>
