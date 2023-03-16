50631e47c01ee6f9b2e2464db21ba4a8539bfc4e
Andrea Coi <andrea.coi@outlook.it>
2023-03-16 16:33:36 +0100

    integrazione asprise demo

--- common/components/ScontrinoHelper.php
+++ common/components/ScontrinoHelper.php
@@ -2,10 +2,11 @@
     namespace common\components;
 
     use yii\web\Response;
+    use yii\helpers\Json;
 
     class ScontrinoHelper {
 
-        public function scanOCRAsprise($nomefile) {
+        public static function scanOCRAsprise($nomefile) {
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, \Yii::$app->params['endpoint_server_aspriseocr']);
             curl_setopt($ch, CURLOPT_POST, true);
@@ -27,8 +28,83 @@
             // associo al contenuto di response la stringa generata precedentemente
             $response = $response_curl;
 
-            curl_close($curl);
+            curl_close($ch);
+            $response = Json::decode($response);
             return $response;
+        }
+
+        // dummy scanner 
+        public static function dummyOCR() {
+            $dummyjson = '{
+                "request_id" : "P_218.186.139.27_kl96a7ie_o9k",
+                "ref_no" : "AspDemo_1613550458205_154",
+                "file_name" : "SG-01s.jpg",
+                "request_received_on" : 1613550460551,
+                "success" : true,
+                "image_width" : 1200,
+                "image_height" : 1600,
+                "image_rotation" : -0.006,
+                "recognition_completed_on" : 1613550461387,
+                "receipts" : [ {
+                  "merchant_name" : "McDonald\'s",
+                  "merchant_address" : "600 @ Toa Payoh #01-02, Singapore 319515",
+                  "merchant_phone" : "62596362",
+                  "merchant_website" : null,
+                  "merchant_tax_reg_no" : "M2-0023981-4",
+                  "merchant_company_reg_no" : null,
+                  "merchant_logo" : "/ocr/api/img/demo/logos/mcd.jpg",
+                  "region" : null,
+                  "mall" : "600 @ Toa Payoh",
+                  "country" : "SG",
+                  "receipt_no" : "002201330026",
+                  "date" : "2016-01-13",
+                  "time" : "15:49",
+                  "items" : [ {
+                    "amount" : 2.95,
+                    "description" : "Med Ice Lemon Tea",
+                    "flags" : "",
+                    "qty" : 1,
+                    "remarks" : null,
+                    "unitPrice" : null
+                  }, {
+                    "amount" : 2.40,
+                    "description" : "Coffee with Milk",
+                    "flags" : "",
+                    "qty" : 1,
+                    "remarks" : null,
+                    "unitPrice" : null
+                  } ],
+                  "currency" : "SGD",
+                  "total" : 5.35,
+                  "subtotal" : null,
+                  "tax" : 0.35,
+                  "service_charge" : null,
+                  "tip" : null,
+                  "payment_method" : "cash",
+                  "payment_details" : null,
+                  "credit_card_type" : null,
+                  "credit_card_number" : null,
+                  "ocr_text" : "         McDonald\'s Toa Payoh Central\n            600 @ Toa Payoh #01-02,\n              Singapore 319515\n                Tel: 62596362\n       McDonald\'s Restaurants Pte Ltd\n          GST REGN NO : M2-0023981-4\n                 TAX INVOICE\n   INV# 002201330026\n  ORD $57 -REG # 1 - 13/01/2016 15:49:52\n  QTY ITEM                            TOTAL\n    1 Med Ice Lemon Tea                2.95\n    1 Coffee with Milk                 2.40\n Eat- In Total (incl GST)              5.35\n Cash Tendered                         10.00\n Change                                 4.65\n TOTAL INCLUDES GST OF                  0.35\n     Thank You and Have A Nice Day",
+                  "ocr_confidence" : 96.60,
+                  "width" : 954,
+                  "height" : 1170,
+                  "avg_char_width" : null,
+                  "avg_line_height" : null,
+                  "source_locations" : {
+                    "date" : [ [ { "y" : 684, "x" : 500 }, { "y" : 681, "x" : 965 }, { "y" : 747, "x" : 966 }, { "y" : 750, "x" : 501 } ] ],
+                    "total" : [ [ { "y" : 957, "x" : 936 }, { "y" : 957, "x" : 1041 }, { "y" : 1012, "x" : 1041 }, { "y" : 1012, "x" : 936 } ] ],
+                    "receipt_no" : [ [ { "y" : 588, "x" : 246 }, { "y" : 587, "x" : 528 }, { "y" : 645, "x" : 528 }, { "y" : 646, "x" : 246 } ] ],
+                    "doc" : [ [ { "y" : 163, "x" : 44 }, { "y" : 157, "x" : 1094 }, { "y" : 1444, "x" : 1102 }, { "y" : 1450, "x" : 52 } ] ],
+                    "merchant_name" : [ [ { "y" : 223, "x" : 254 }, { "y" : 215, "x" : 876 }, { "y" : 260, "x" : 877 }, { "y" : 269, "x" : 255 } ] ],
+                    "tax" : [ [ { "y" : 1200, "x" : 949 }, { "y" : 1201, "x" : 1057 }, { "y" : 1258, "x" : 1056 }, { "y" : 1257, "x" : 948 } ] ],
+                    "merchant_address" : [ [ { "y" : 262, "x" : 318 }, { "y" : 261, "x" : 832 }, { "y" : 309, "x" : 832 }, { "y" : 309, "x" : 318 } ], [ { "y" : 308, "x" : 383 }, { "y" : 304, "x" : 768 }, { "y" : 356, "x" : 769 }, { "y" : 361, "x" : 384 } ] ],
+                    "merchant_phone" : [ [ { "y" : 354, "x" : 539 }, { "y" : 354, "x" : 719 }, { "y" : 396, "x" : 719 }, { "y" : 397, "x" : 539 } ] ],
+                    "merchant_tax_reg_no" : [ [ { "y" : 444, "x" : 574 }, { "y" : 438, "x" : 856 }, { "y" : 482, "x" : 857 }, { "y" : 488, "x" : 575 } ] ]
+                  }
+                } ]
+              }';
+              $response = Json::decode($dummyjson);
+              return $response;
         }
 
         // scanner tesseract Alessandro Testa - da dismettere e da rimpiazzare successivamente
