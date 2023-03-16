54691a47b50658285d803d21d8e77c124e2c5059
Andrea Coi <andrea.coi@outlook.it>
2023-03-16 16:35:01 +0100

    creazione vista scontrino

--- frontend/modules/dashboard/views/scontrino/view.php
+++ frontend/modules/dashboard/views/scontrino/view.php
@@ -2,22 +2,64 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
-use yii\widgets\GridView;
+use yii\grid\GridView;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Scontrino */
+/* @var $datamodel common\models\ScontrinoData */
+/* @var $dataprodottimodel common\models\ProdottiScontrinoData */
 
-$this->title = 'Scontrino n°'. $model->id;
+$this->title = 'Scontrino id n°'. $model->id;
 
 \yii\web\YiiAsset::register($this);
 ?>
+<div class="alert alert-success">
+    <h1>Questo scontrino vale 10 punti!</h1>
+</div>
+<hr />
+<h1>Dettaglio scontrino</h1>
 <div class="scontrino-view container">
-
-    <h1><?= Html::encode($this->title) ?></h1>
+<h3 class="esercente title">Dati immagine scansionata</h3>
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
             'nomefile',
+            'data_caricamento',
         ],
     ]) ?>
+    <h3 class="esercente title">Dati esercente</h3>
+    <?= DetailView::widget([
+        'model' => $datamodel,
+        'attributes' => [
+            'rfscontrino',
+            'numerodocumento',
+            'dataemissione',
+            'ragionesociale',
+            'indirizzo',
+            'provincia',
+            'citta',
+            'cap',
+            'telefono',
+            'partitaiva',
+        ],
+    ]) ?>
+    <?= GridView::widget([
+        'dataProvider' => $provider_dataprodotti,
+		'emptyText' => 'Non esistono prodotti sullo scontrino.',
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            [ 
+                'header' => 'Nome prodotto',
+                'attribute' => 'nomeprodotto',
+            ],
+            [
+                'header' => 'Prezzo Prodotto',
+                'attribute' => 'prezzo_prodotto',
+            ],
+            [
+                'header' => 'Numero punti',
+                'attribute' => 'numeropunti',
+            ],
+        ]]);
+        ?>
 </div>
