ab0e09355b5c19bad2375ea96a9ba4fa39452615
Andrea Coi <andrea.coi@outlook.it>
2023-03-24 08:36:24 +0100

    pubblicazione report corretti

--- backend/views/statistiche/prodotti.php
+++ backend/views/statistiche/prodotti.php
@@ -4,9 +4,9 @@
     /* @var $this yii\web\View */
     /* @var $dataProvider yii\data\ActiveDataProvider */
 
-    $this->title = "Prodotti";
+    $this->title = "Statistiche Prodotti";
     $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="statistiche-index">
-<iframe title="Test_interviste" width="100%" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiZjdjNTlmY2YtMzcyNS00ZTU1LWI2OTUtZTg3ZmMwMDQwZGM1IiwidCI6IjY1NGViNGY3LTc1YjAtNGQzNi04Y2E2LTU5NGVjOTAwZWJkMSIsImMiOjl9&pageName=ReportSection8d20f514634df2c071ae" frameborder="0" allowFullScreen="true"></iframe>
+<iframe title="Statistiche Prodotti" width="100%" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiZDhkN2YwYTYtNDJiNC00M2M3LWJhOTEtMjU1ZDViYmY4YjNhIiwidCI6IjY1NGViNGY3LTc1YjAtNGQzNi04Y2E2LTU5NGVjOTAwZWJkMSIsImMiOjl9" frameborder="0" allowFullScreen="true"></iframe>
 </div>
