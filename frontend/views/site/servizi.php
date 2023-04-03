20b8258ddbd4b70af2bc2d75341339d193af2b67
Andrea Coi <andrea.coi@outlook.it>
2023-04-03 15:35:39 +0200

    inserisce pagina servizi

---
+++ frontend/views/site/servizi.php
@@ -0,0 +1,177 @@
+<?php
+   /** @var yii\web\View $this */
+   
+   use yii\helpers\Html;
+   
+   $this->title = 'Servizi Profility';
+   ?>
+<div class="site-about container">
+   <div class="container-fluid">
+      <div class="row">
+         <div class="col">
+            <div class="page-title mt-5">
+               <h1><?= Html::encode($this->title) ?></h1>
+            </div>
+         </div>
+      </div>
+      <div class="row">
+         <div class="col">
+            <div id="servizi" class="page-content mt-5">
+            <ol>
+   <li>
+      Gestione attività fidelity
+      <ol type="a">
+         <li>
+            Personalizzazione app per il cliente
+         </li>
+         <li>
+            Gestione accumulo punti
+         </li>
+         <li>
+            Richiesta premi
+         </li>
+         <li>
+            Proiezione premi richiesti dai consumatori
+         </li>
+         <li>
+            Gestione campagne promozionali sugli acquisti dei prodotti
+         </li>
+         <li>
+            Gestione notifiche push
+         </li>
+      </ol>
+   </li>
+   <li>
+      Comunicazione attività fidelity
+      <ol type="a">
+         <li>
+            Campagna social media marketing
+         </li>
+         <li>
+            “Regalo” punti in caso di condivisione social con TAG e HASTAG
+         </li>
+         <li>
+            Campagna comunicazione promozione in essere
+         </li>
+         <li>
+            Gestione segnalazioni da parte degli utenti 
+         </li>
+      </ol>
+   </li>
+   <li>
+      Gestione statistiche
+      <ol type="a">
+         <li>
+            Consumatori
+            <ol type="i">
+               <li>
+                  Profilazione socio-demo consumatori
+               </li>
+               <li>
+                  Profilazione comportamentale consumatori
+               </li>
+               <li>
+                  Profilazione acquisti dei consumatori: abitudini, tipologia di referenze, pdv frequentati
+               </li>
+               <li>
+                  Individuazione brand addicted
+               </li>
+            </ol>
+         </li>
+         <li>
+            Clienti GDO
+            <ol type="i">
+               <li>
+                  Penetrazione della distribuzione delle referenze presso i clienti GDO
+               </li>
+               <li>
+                  KPI su attività in-store
+               </li>
+               <li>
+                  KPI su attività promozionali
+               </li>
+               <li>
+                  Segnalazioni da parte dei consumatori: es. rotture di stock, confezioni danneggiate, richiesta di referenze non presente nel pdv
+               </li>
+            </ol>
+         </li>
+      </ol>
+   </li>
+   <li>
+      Strategia competitiva azienda
+      <ol type="a">
+         <li>
+            Prezzi praticati dai clienti Retail e/o Ho.re.ca.
+         </li>
+         <li>
+            Variazione prezzi nel tempo
+         </li>
+         <li>
+             
+         </li>
+      </ol>
+   </li>
+   <li>
+      Strategia competitiva competitor
+      <ol type="a">
+         <li>
+            Promo volantino competitor
+            <ol type="i">
+               <li>
+                  Scouting attività promo su volantini dei competitor
+               </li>
+               <li>
+                  Individuazione Insegne GDO coinvolte
+               </li>
+               <li>
+                  Tipo di attività (taglio prezzo; Esposizione preferenziale; Lancio nuovi prodotti ecc…)
+               </li>
+            </ol>
+         </li>
+         <li>
+            Promo prezzo dei competitor
+            <ol type="i">
+               <li>
+                  Invio notifica ai consumatori che frequentano 
+                  <ol>
+                     <li>
+                        abitualmente il pdv 
+                     </li>
+                     <li>
+                        l’insegna coinvolta 
+                     </li>
+                     <li>
+                        il comune di residenza
+                     </li>
+                  </ol>
+               </li>
+            </ol>
+         </li>
+      </ol>
+   </li>
+</ol>
+con l’avviso di aumento dei punti concessi in caso di acquisto
+<ol start="6">
+   <li>
+      Gestione marketing research
+      <ol type="a">
+         <li>
+            Realizzazione di ricerche ad hoc in concomitanza di degustazioni/attività in store tramite checkpoint 
+         </li>
+         <li>
+            Realizzazione Customer Satisfaction
+         </li>
+         <li>
+            Realizzazione marketing research per lancio nuovi prodotti, potenziale di mercato
+         </li>
+         <li>
+            Realizzazione marketing research qualitative 
+         </li>
+      </ol>
+   </li>
+</ol>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
