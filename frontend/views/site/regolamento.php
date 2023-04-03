292c9b0209ef8cff9d2b7cd1226d570b14158b72
Andrea Coi <andrea.coi@outlook.it>
2023-04-03 15:35:24 +0200

    inserisce regolamento demo per presentazione

--- frontend/views/site/regolamento.php
+++ frontend/views/site/regolamento.php
@@ -1,30 +1,127 @@
 <?php
-
-/** @var yii\web\View $this */
-
-use yii\helpers\Html;
-
-$this->title = 'Regolamento';
-?>
+   /** @var yii\web\View $this */
+   
+   use yii\helpers\Html;
+   
+   $this->title = 'Regolamento';
+   ?>
 <div class="site-about container">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col">
-                <div class="page-title">
-                    <h1><?= Html::encode($this->title) ?></h1>
-                </div>
+   <div class="container-fluid">
+      <div class="row">
+         <div class="col">
+            <div class="page-title mt-5">
+               <h1><?= Html::encode($this->title) ?></h1>
             </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <div class="page-content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras a orci in lacus faucibus porttitor. Nulla interdum, erat id pretium tincidunt, diam quam tristique nisi, vitae convallis nisi dui id nibh. Phasellus vehicula enim nisl, sit amet dapibus risus vestibulum sit amet. Curabitur aliquam nec mi sit amet dictum. Nulla tincidunt laoreet sem, quis convallis diam pretium vel. Duis et sagittis diam. Quisque vulputate facilisis luctus.</p>
-                <p>Phasellus ultrices nisl eget justo dignissim, in porta metus porttitor. Suspendisse pretium, nibh iaculis tempor convallis, massa dolor bibendum dolor, eu congue tortor mi vel eros. Nam tortor mauris, semper eget mollis at, lobortis et dolor. Quisque tincidunt tellus non pharetra aliquam. Vestibulum vel quam ex. Nullam bibendum orci ex, vitae suscipit felis porttitor sit amet. Phasellus vel leo nisl. Etiam fringilla eros aliquet sem feugiat, sed commodo turpis dapibus. Curabitur pellentesque ante vel tempor luctus. Sed eget mollis arcu. Quisque semper in massa a aliquam.</p>
-                <p>Nam luctus eros purus, in vulputate arcu mollis molestie. Donec diam mauris, ultrices in pulvinar in, lobortis sed dui. Fusce tincidunt fermentum nisl, ac volutpat velit elementum ut. Morbi in enim eget urna iaculis molestie. Maecenas porta metus eu nisl suscipit, ut pellentesque odio euismod. Sed luctus volutpat urna quis finibus. Nam ultricies orci feugiat nunc vulputate, nec pellentesque mauris tempus. Cras rutrum vitae enim nec maximus. Sed rutrum volutpat aliquet.</p>
-                <p>Integer scelerisque mattis sodales. Quisque cursus metus nibh, sed rhoncus nisi efficitur id. Nam ac rutrum orci. Donec cursus nulla eget nibh consequat, ut porttitor est fringilla. Fusce iaculis mauris auctor neque iaculis, vel iaculis elit porta. Integer ultrices in sem quis convallis. Morbi condimentum vulputate sollicitudin. Phasellus tempus pellentesque nunc. Integer tempor metus nec augue pulvinar cursus. Vivamus vitae vestibulum enim, a rutrum mauris. Donec sit amet venenatis neque. Aenean interdum placerat urna sit amet sollicitudin.</p>
-                <p>Duis nulla nisl, blandit quis sapien eu, dignissim feugiat sapien. Proin augue diam, laoreet eu facilisis ut, tristique quis orci. Cras tincidunt urna quis quam malesuada vulputate. Pellentesque suscipit, erat facilisis accumsan lacinia, mi justo iaculis erat, id maximus neque massa sit amet massa. Etiam mattis faucibus ornare. Morbi scelerisque ipsum eu augue vulputate, a suscipit libero mattis. Proin non consectetur massa. Donec interdum leo et dictum iaculis. Praesent cursus sapien non augue cursus interdum.</p>
-                </div>
+         </div>
+      </div>
+      <div class="row">
+         <div class="col">
+            <div id="regolamento" class="page-content mt-5">
+               <h2>REGOLAMENTO RACCOLTA PUNTI PROFILITY</h2>
+               <ol class="master">
+                  <li>
+                     <h3>Chi può partecipare</h3>
+                  </li>
+               </ol>
+               <p>La raccolta punti è aperta a tutti i consumatori maggiorenni residenti in Italia. La partecipazione è gratuita e non è richiesto alcun costo per partecipare.</p>
+               <ol class="master" start="2">
+                  <li>
+                     <h3>AMBITO TERRITORIALE E TEMPORALE</h3>
+                  </li>
+               </ol>
+               <p>La raccolta punti è valida esclusivamente per gli acquisti effettuati in Italia presso i punti vendita in cui sono distribuiti e commercializzati i prodotti a marchio [NOME DEL BRAND]. Il programma ha inizio il [DATA DI INIZIO] e termina il [DATA DI FINE] salvo proroghe o modifiche comunicate dal [NOME DEL BRAND].</p>
+               <ol class="master" start="3">
+                  <li>
+                     <h3>PRODOTTI PARTECIPANTI</h3>
+                  </li>
+               </ol>
+               <p>Partecipano alla raccolta punti tutti i prodotti del [NOME DEL BRAND] che esibiscono il logo dell’azienda.</p>
+               <ol class="master" start="4">
+                  <li>
+                     <h3>MODALITÀ DI PARTECIPAZIONE</h3>
+                  </li>
+               </ol>
+               <p>Per partecipare alla raccolta punti, è necessario effettuare acquisti di almeno uno dei prodotti partecipanti presso un punto vendita autorizzato [NOME DEL BRAND].</p>
+               <p>I punti si accumulano inviando la foto dello scontrino comprovante l’acquisto dei prodotti partecipanti alla raccolta. Ogni scontrino può essere inviato una ed una sola volta. In caso di invio dello stesso scontrino più volte l’azienda promotrice si riserva la facoltà di escludere il partecipante dalla raccolta punti con l’azzeramento dei punti eventualmente accumulati. Nel caso in cui lo stesso scontrino sia inviato da più partecipanti, l’azienda si riserva la facoltà di escludere tutti i soggetti coinvolti.</p>
+               <p>Per essere valido, lo scontrino fiscale deve contenere necessariamente i seguenti elementi:</p>
+               <ol>
+                  <li>
+                    La ragione sociale, partita iva, indirizzo del luogo di acquisto del prodotto;
+                  </li>
+                  <li>
+                    Il numero dello scontrino fiscale;
+                  </li>
+                  <li>
+                    Il numero univoco <em>RT</em>;
+                  </li>
+                  <li>
+                    La data e l’ora di acquisto;
+                  </li>
+                  <li>
+                    Il prodotto/i del [NOME DEL BRAND] partecipante/i all’iniziativa.
+                  </li>
+               </ol>
+               <p>In mancanza di uno solo degli elementi sopra elencati, lo scontrino non sarà considerato valido per l’accumulo punti.</p>
+               <p>Per partecipare al programma Fidelity l'utente dovrà registrarsi sul sito www.nomedelbrand.profility.it accettando in ogni sua parte le condizioni del programma fornendo, inoltre, i propri dati anagrafici, il numero di telefono, l'indirizzo email e altre informazioni richieste per la partecipazione.</p>
+               <ol class="master" start="5">
+                  <li>
+                     <h3>PUNTEGGI E PREMI</h3>
+                  </li>
+               </ol>
+               <p>Ogni prodotto partecipante ha un valore in punti assegnato dal [NOME DEL BRAND]. Il valore in punti è presente all’interno del sito internet www.nomebrand.fidelity.it. Gli utenti potranno accumulare punti per ogni acquisto e successivamente convertirli in premi, che saranno comunicati sul sito [SITO WEB DEL PROGRAMMA DI FIDELIZZAZIONE] o tramite l'apposita app [NOME DELL'APP]. I premi potranno essere soggetti a limitazioni o restrizioni. I Punti accumulati non possono essere ceduti a terzi partecipanti alla medesima raccolta punti.</p>
+               <ol class="master" start="6">
+                  <li>
+                     <h3>SOGLIE PUNTI PER PREMI</h3>
+                  </li>
+               </ol>
+               <p> </p>
+               <ol class="master" start="7">
+                  <li>
+                     <h3>CONDIZIONI GENERALI</h3>
+                  </li>
+               </ol>
+               <p>Il [NOME DEL BRAND] si riserva il diritto di sospendere, modificare o annullare il programma di raccolta punti in qualsiasi momento, comunicando tempestivamente gli aggiornamenti sul sito web del programma di fidelizzazione e tramite la app [NOME DELL'APP]. Gli utenti sono tenuti a rispettare le condizioni del presente regolamento e a fornire informazioni veritiere e aggiornate al momento dell'iscrizione al programma. Il [NOME DEL BRAND] non sarà responsabile per eventuali danni o perdite derivanti da un utilizzo improprio o fraudolento del programma di raccolta punti.</p>
+               <ol class="master" start="8">
+                  <li>
+                     <h3>TUTELA DEI DATI PERSONALI</h3>
+                  </li>
+               </ol>
+               <p>Ai sensi del Regolamento (UE) 2016/679 del Parlamento europeo e del Consiglio del 27 aprile 2016 (GDPR) relativo alla protezione delle persone fisiche con riguardo al trattamento dei dati personali, il brand [Nome del brand] informa che i dati personali forniti dall'utente nell'ambito della raccolta punti saranno trattati nel rispetto della normativa sulla privacy e con le seguenti finalità:</p>
+               <ul>
+                  <li>
+                    gestione della raccolta punti;
+                  </li>
+                  <li>
+                    invio di comunicazioni promozionali e commerciali relative ai prodotti e servizi del brand [Nome del brand] e di altre aziende del gruppo;
+                  </li>
+                  <li>
+                    elaborazione di studi di mercato e analisi di dati a fini statistici e di profilazione di marketing.
+                  </li>
+               </ul>
+               <p>Il trattamento dei dati personali avverrà con modalità informatiche e/o telematiche, con strumenti idonei a garantire la sicurezza e la riservatezza dei dati stessi, e sarà effettuato dal personale del brand e da eventuali responsabili esterni nominati dal brand.</p>
+               <p>Il conferimento dei dati personali è necessario per partecipare alla raccolta punti e per ricevere le relative comunicazioni commerciali e promozionali. L'eventuale rifiuto di fornire i dati personali comporterà l'impossibilità di partecipare alla raccolta punti e di ricevere le comunicazioni commerciali e promozionali.</p>
+               <p>I dati personali dell'utente potranno essere comunicati a terzi, quali fornitori di servizi tecnologici, di elaborazione dati e di assistenza, per il corretto svolgimento delle attività connesse alla raccolta punti e alle finalità di profilazione di marketing.</p>
+               <p>In ogni momento, l'utente potrà esercitare i diritti previsti dal GDPR, tra cui:</p>
+               <ul>
+                  <li>
+                    diritto di accesso ai propri dati personali;
+                  </li>
+                  <li>
+                    diritto di rettifica, cancellazione e limitazione del trattamento dei propri dati personali;
+                  </li>
+                  <li>
+                    diritto di opposizione al trattamento dei propri dati personali;
+                  </li>
+                  <li>
+                    diritto alla portabilità dei propri dati personali;
+                  </li>
+                  <li>
+                    diritto di revocare il consenso al trattamento dei propri dati personali.
+                  </li>
+               </ul>
+               <p>Per esercitare tali diritti o per ricevere informazioni sul trattamento dei propri dati personali, l'utente potrà contattare il brand tramite l'apposita sezione del sito web o inviando una comunicazione scritta all'indirizzo [Indirizzo del brand].</p>
             </div>
-        </div>
-    </div>
+         </div>
+      </div>
+   </div>
 </div>
