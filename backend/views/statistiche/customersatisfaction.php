1294432837035f8dad7b11fea5b61febcb5b5a9d
Andrea Coi <andrea.coi@outlook.it>
2023-04-03 10:39:54 +0200

    cambio nomi a viste

--- backend/views/statistiche/customersatisfaction.php
+++ backend/views/statistiche/customersatisfaction.php
@@ -8,5 +8,5 @@
     $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="statistiche-index">
-<iframe title="Customer Satisfaction" width="100%" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiMzVhNDQ4NDEtNTI1ZS00MDdkLWFhY2ItYWMyOWZhNjQwZGU1IiwidCI6IjY1NGViNGY3LTc1YjAtNGQzNi04Y2E2LTU5NGVjOTAwZWJkMSIsImMiOjl9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
+<iframe title="Customer Satisfaction" width="100%" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiZjdjNTlmY2YtMzcyNS00ZTU1LWI2OTUtZTg3ZmMwMDQwZGM1IiwidCI6IjY1NGViNGY3LTc1YjAtNGQzNi04Y2E2LTU5NGVjOTAwZWJkMSIsImMiOjl9&pageName=ReportSection8d20f514634df2c071ae" frameborder="0" allowFullScreen="true"></iframe>
 </div>
---
+++ backend/views/statistiche/positioninganalysis.php
@@ -0,0 +1,12 @@
+<?php
+    use yii\helpers\Html;
+    use yii\helpers\Url;
+    /* @var $this yii\web\View */
+    /* @var $dataProvider yii\data\ActiveDataProvider */
+
+    $this->title = "Positioning Analysis";
+    $this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="statistiche-index">
+<iframe title="Positioning Analysis" width="100%" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiMjA2OWM3OTQtYzRlYy00NjIyLTkwNDQtN2M3YWYzMWM4NDU4IiwidCI6IjY1NGViNGY3LTc1YjAtNGQzNi04Y2E2LTU5NGVjOTAwZWJkMSIsImMiOjl9&pageName=ReportSection8d20f514634df2c071ae" frameborder="0" allowFullScreen="true"></iframe>
+</div>
