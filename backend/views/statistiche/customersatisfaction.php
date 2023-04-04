478b0248b7dc592b41db89d6bc2ae24b4e3ed777
Andrea Coi <andrea.coi@outlook.it>
2023-04-04 09:38:17 +0200

    sostituisce grafici customer satisfaction

--- backend/views/statistiche/customersatisfaction.php
+++ backend/views/statistiche/customersatisfaction.php
@@ -8,5 +8,5 @@
     $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="statistiche-index">
-<iframe title="customer-satisfaction" width="100%" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiMzVhNDQ4NDEtNTI1ZS00MDdkLWFhY2ItYWMyOWZhNjQwZGU1IiwidCI6IjY1NGViNGY3LTc1YjAtNGQzNi04Y2E2LTU5NGVjOTAwZWJkMSIsImMiOjl9" frameborder="0" allowFullScreen="true"></iframe>
+<iframe title="customer-satisfaction" width="100%" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiZDNjOTA0MWItYTNiOC00MDM3LTgyNGQtNzllYzY3NGVjMTUyIiwidCI6IjY1NGViNGY3LTc1YjAtNGQzNi04Y2E2LTU5NGVjOTAwZWJkMSIsImMiOjl9" frameborder="0" allowFullScreen="true"></iframe>
 </div>
