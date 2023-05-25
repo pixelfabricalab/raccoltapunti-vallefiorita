e5526f09b839d80c1753d5ab2e7b6e6418091ddc
Andrea Coi <andrea.coi@outlook.it>
2023-05-25 10:04:05 +0200

    forza l'aggiornamento della favicon

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -29,23 +29,23 @@ $this->registerJs("const apiUrl = '" . Yii::getAlias('@web/api') . "';", View::P
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
-
-    <link rel="apple-touch-icon" sizes="57x57" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="<?= \Yii::getAlias('@web') ?>/images/favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= \Yii::getAlias('@web') ?>/images/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?= \Yii::getAlias('@web') ?>/images/favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= \Yii::getAlias('@web') ?>/images/favicon/favicon-16x16.png">
-    <link rel="manifest" href="<?= \Yii::getAlias('@web') ?>/images/favicon/manifest.json">
+    <?php $randver = rand(1,45);?>
+    <link rel="apple-touch-icon" sizes="57x57" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-57x57.png?v=<?php echo $randver; ?>">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-60x60.png?v=<?php echo $randver; ?>">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-72x72.png?v=<?php echo $randver; ?>">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-76x76.png?v=<?php echo $randver; ?>">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-114x114.png?v=<?php echo $randver; ?>">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-120x120.png?v=<?php echo $randver; ?>">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-144x144.png?v=<?php echo $randver; ?>">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-152x152.png?v=<?php echo $randver; ?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= \Yii::getAlias('@web') ?>/images/favicon/apple-icon-180x180.png?v=<?php echo $randver; ?>">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?= \Yii::getAlias('@web') ?>/images/favicon/android-icon-192x192.png?v=<?php echo $randver; ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= \Yii::getAlias('@web') ?>/images/favicon/favicon-32x32.png?v=<?php echo $randver; ?>">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?= \Yii::getAlias('@web') ?>/images/favicon/favicon-96x96.png?v=<?php echo $randver; ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= \Yii::getAlias('@web') ?>/images/favicon/favicon-16x16.png?v=<?php echo $randver; ?>">
+    <link rel="manifest" href="<?= \Yii::getAlias('@web') ?>/images/favicon/manifest.json?v=<?php echo $randver; ?>">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="<?= \Yii::getAlias('@web') ?>/images/favicon/ms-icon-144x144.png">
+    <meta name="msapplication-TileImage" content="<?= \Yii::getAlias('@web') ?>/images/favicon/ms-icon-144x144.png?v=<?php echo $randver; ?>">
     <meta name="theme-color" content="#ffffff">
 
     <?php $this->head() ?>
