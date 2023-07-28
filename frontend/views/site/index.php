a430eaea8fdad383554d981e1883b0a22f225fa1
Andrea Coi <andrea.coi@outlook.it>
2023-07-28 11:47:04 +0200

    inserisce link su inizia subito

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -11,7 +11,7 @@ $this->title = 'Raccolta punti - Demo App';
         <div class="herotext">
             <h1 class="display-4 hero-title">Raccolta punti</h1>
             <p class="lead fw-normal hero-desc">Una raccolta punti non è mai stata così semplice.<br />Inquadra lo scontrino, scatta una foto e inviala con l'applicazione!</p>
-            <a class="btn btn-profility" href="#">Inizia subito</a>
+            <a class="btn btn-profility" href="<?= Url::toRoute('//dashboard/scontrino/create') ?>">Inizia subito</a>
         </div>
     </div>
   </div>
