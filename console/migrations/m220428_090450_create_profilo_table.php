614b981442c6f3571f461b7e08987c74bca65a42
Vincent <vincent.veri@gmail.com>
2023-10-17 16:14:08 +0200

    correggi creazione tablela profilo

--- console/migrations/m220428_090450_create_profilo_table.php
+++ console/migrations/m220428_090450_create_profilo_table.php
@@ -23,8 +23,29 @@ class m220428_090450_create_profilo_table extends Migration
             'residenza_citta' => $this->string(),
             'residenza_cap' => $this->string(),
             'residenza_provincia' => $this->string(),
+            'email' => $this->string(),
             'cellulare' => $this->string(),
+            'user_id' => $this->integer()->notNull(),
+            'creato_il' => $this->dateTime(),
+            'modificato_il' => $this->dateTime(),
         ]);
+
+        // creates index for column `user_id`
+        $this->createIndex(
+            '{{%idx-profilo-user_id}}',
+            '{{%profilo}}',
+            'user_id'
+        );
+
+        // add foreign key for table `{{%user}}`
+        $this->addForeignKey(
+            '{{%fk-profilo-user_id}}',
+            '{{%profilo}}',
+            'user_id',
+            '{{%user}}',
+            'id',
+            'CASCADE'
+        );
     }
 
     /**
