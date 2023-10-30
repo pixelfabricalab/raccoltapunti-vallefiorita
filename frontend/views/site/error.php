1e186a31ff42a12ea677824f178fd62df7183014
Vincent <vincent.veri@gmail.com>
2023-10-30 17:40:48 +0100

    cambia schermata di errore

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -11,17 +11,12 @@ $this->title = $name;
 ?>
 <div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
     <div class="alert alert-danger">
         <?= nl2br(Html::encode($message)) ?>
     </div>
 
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
+    <div style="max-width: 340px;">
+        <div class="tenor-gif-embed" data-postid="12674712" data-share-method="host" data-aspect-ratio="1.78" data-width="100%"><a href="https://tenor.com/view/monkey-computer-not-working-computer-broken-computer-problems-computer-issues-gif-12674712">Monkey Computer Not Working GIF</a>from <a href="https://tenor.com/search/monkey-gifs">Monkey GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+    </div>
 
 </div>
