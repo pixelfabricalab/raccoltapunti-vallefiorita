40660232393f1e095b031c04bd680ed442091046
Vincent <vincent.veri@gmail.com>
2023-10-31 16:28:10 +0100

    cambia tag codice coupon

--- frontend/modules/dashboard/views/coupon/_single.php
+++ frontend/modules/dashboard/views/coupon/_single.php
@@ -8,7 +8,7 @@
                 <div class="col">
                     <div class="p-4 py-md-3 px-md-0">
                     <h3>Valore: <span class="text-success"><?= $model->etichettaValore ?></span></h3>
-                    <h6><span class="text-muted">CODICE:</span> <?= $model->codice ?></h6>
+                    <code><span class="text-muted">CODICE:</span> <?= $model->codice ?></code>
                     <ul class="list-unstyled mb-0">
                         <li>Stato: <?= ($model->status) ? '<span class="text-success">ATTIVO</span>' : '<span class="text-danger font-weight-bold">UTILIZZATO</span>' ?></li>
                         <?php if (!$model->status) : ?>
