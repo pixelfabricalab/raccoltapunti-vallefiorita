53580b9f300c4db31f966da6d338e614b0023b7a
Vincent <vincent.veri@gmail.com>
2023-11-06 17:36:33 +0100

    rimuovi testo inutile

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -13,8 +13,6 @@ $this->title = 'Login';
     <div class="mt-5 offset-lg-3 col-lg-6">
         <h1><?= Html::encode($this->title) ?></h1>
 
-        <p>Please fill out the following fields to login:</p>
-
         <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
             <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
