4f5dc7fea3afb41f773b9de63d29e2eea97d2252
Vincent <vincent.veri@gmail.com>
2023-11-06 10:49:50 +0100

    fix titolo regolamento

--- frontend/views/site/regolamento.php
+++ frontend/views/site/regolamento.php
@@ -10,7 +10,7 @@
       <div class="row">
          <div class="col">
             <div class="page-title mt-5">
-               <h5><?= Html::encode($this->title) ?></h5>
+               <h3><?= Html::encode($this->title) ?></h3>
             </div>
          </div>
       </div>
